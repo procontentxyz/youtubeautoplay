ec900c776b223979116a7d5c163d70c93af4d49b
procontentxyz <70431728+procontentxyz@users.noreply.github.com>
2022-01-09 12:08:59 +0530

    Update and rename main-updt-4.js to main-updt-6.js

--- js/main-updt-6.js
+++ js/main-updt-6.js
@@ -6,17 +6,13 @@ var pausedVideo = "";
 window.onYouTubeIframeAPIReady = function() {
     $('.youtube-video').each(function(){
         var getYtID = $(this).data('video-id');
-        var getHeight = $(this).data('video-height');
         var getAutoPlay = $(this).data('video-autoplay');
-        if(!getHeight){
-            getHeight =  "360";
-        }
 
         iterator++;
         $(this).attr('id', 'player'+iterator);
         players.push(createPlayer({
             id: 'player'+iterator,
-            height: getHeight,
+            height: '100%',
             width: '100%',
             videoId: getYtID,
             events: {
@@ -33,12 +29,12 @@ window.onYouTubeIframeAPIReady = function() {
                 if(isElementInViewport(that)){
                     $('.youtube-video').each(function (i) {
                         var getAutoPlay = $(this).data('autoplay');
-                        if($(this).attr('id') === $(that).attr('id') && $(this).attr('id') !== stopVideo && getAutoPlay == "1"){
-                            players[i].mute();
+                        if($(this).attr('id') === $(that).attr('id') && $(this).attr('id') !== pausedVideo && getAutoPlay == "1"){
                             players[i].playVideo();
+                            players[i].mute();
                         }else{
-                            players[i].stopVideo();
-                            //players[i].pauseVideo();
+                            //players[i].stopVideo();
+                            players[i].pauseVideo();
                         }
                     });
                 }
